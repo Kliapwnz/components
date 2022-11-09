0f61a00abc1298f9dc317c15e3e82b27f903c8c0
Evgeniy Klimovch <kliasanik@gmail.com>
2022-11-09 16:19:22 +0300

    Add Footer.tsx to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import './App.css';
 import {Header} from "./site/Header";
 import {Body} from "./site/Body";
+import {Footer} from "./site/Footer";
 
 function App() {
     return (
-        <div>
+        <>
             <Header/>
             <Body/>
-        </div>
+            <Footer/>
+        </>
 
     );
 }
