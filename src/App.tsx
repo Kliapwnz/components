4720286abca4f7457b319aa6d488339f1e4feae4
Evgeniy Klimovch <kliasanik@gmail.com>
2022-11-09 16:28:15 +0300

    Add Home work

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
     return (
         <>
             <Header title={"New BODY"}/>
-            <Body/>
+            <Body titleForBody={"New BODY"}/>
             <Footer/>
         </>
 
--- src/site/Body.tsx
+++ src/site/Body.tsx
@@ -1,5 +1,9 @@
-export const Body = () => {
+type BodyPropsType = {
+    titleForBody: string
+}
+
+export const Body = (props: BodyPropsType) => {
     return (
-        <div>Body</div>
+        <div>{props.titleForBody}</div>
     )
 }
