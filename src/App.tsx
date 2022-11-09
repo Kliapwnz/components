16dc425e6acefd4720352ca773360adb8a59077f
Evgeniy Klimovch <kliasanik@gmail.com>
2022-11-09 16:10:27 +0300

    Add Header to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Header} from "./site/Header";
 
 function App() {
     return (
-        <div>Hello world!</div>
+        <Header/>
+
     );
 }
 
